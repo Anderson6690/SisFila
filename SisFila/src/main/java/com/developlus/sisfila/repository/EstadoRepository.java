97fb5bd7f07e7632911e7e5efc90da8051d7b865
Alysson997 <madridalysson@gmail.com>
2017-10-22 15:29:31 -0300

    Desenvolvido a filtragem de estados

--- SisFila/src/main/java/com/developlus/sisfila/repository/EstadoRepository.java
+++ SisFila/src/main/java/com/developlus/sisfila/repository/EstadoRepository.java
@@ -2,7 +2,9 @@ package com.developlus.sisfila.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.developlus.sisfila.model.Estado;
+import com.developlus.sisfila.repository.helper.EstadoQueries;
+import com.developlus.sisfila.repository.helper.FuncionarioQueries;
 
-public interface EstadoRepository extends JpaRepository<Estado, Long> {
+public interface EstadoRepository extends JpaRepository<Estado, Long>, EstadoQueries {
 
 }
