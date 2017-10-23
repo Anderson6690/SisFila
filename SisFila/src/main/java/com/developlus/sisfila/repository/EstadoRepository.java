591e4806ece48357faabf19d6bb5c5b28c9234ca
alysson <madridalysson@gmail.com>
2017-10-23 19:19:00 -0300

    Commit

--- SisFila/src/main/java/com/developlus/sisfila/repository/EstadoRepository.java
+++ SisFila/src/main/java/com/developlus/sisfila/repository/EstadoRepository.java
@@ -1,9 +1,9 @@
 package com.developlus.sisfila.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.developlus.sisfila.model.Estado;
 import com.developlus.sisfila.repository.helper.EstadoQueries;
-import com.developlus.sisfila.repository.helper.FuncionarioQueries;
 
 public interface EstadoRepository extends JpaRepository<Estado, Long>, EstadoQueries {
 
